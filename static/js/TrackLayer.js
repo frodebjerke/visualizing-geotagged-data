b623bcb7cc29444735616560cc74400a77a9fcfd
Frederik Claus <f.v.claus@googlemail.com>
2013-03-14 13:15:47 +0100

    geo: Cleaned up shortest trace layer js

--- static/js/TrackLayer.js
+++ static/js/TrackLayer.js
@@ -1,20 +1,34 @@
-TrackLayer = function(map){
-   this.map  = map;
-   var instance = this;
+/*global $, _V_, OpenLayers, MapLayer, styles, assert, assertTrue, geographic, onVideoProgress, Trace */
 
-   $video = $("#video");
-   width = $video.width();
-   height = $video.height();
-   
+"use strict";
+
+/**
+ * @public
+ * @author Frederik Claus <f.v.claus@googlemail.com>
+ * @description Displays a trace between two points on the graph and hightlights the progress
+ * @param {OpenLayers.Map} map
+ */
+var TrackLayer = function(map){
+   assert(map.CLASS_NAME, "OpenLayers.Map");
+
+   var instance = this,
+       $video = $("#video"),
+       width = $video.width(),
+       height = $video.height();
+
+   this.map  = map;
    this.player =  _V_("video");
    //mute player
    this.player.volume(0);
    this.player.size(width,height);
 
-
+   // start playing as soon as the video metadata is fully loaded
+   // update the current position of the trace
    this.player.addEvent("loadeddata",function(){
       //this is the video object
-      this.addEvent("timeupdate",trackLayer.highlightUpdate);
+      this.addEvent("timeupdate", function () {
+         instance._highlightUpdate.apply(instance);
+      });
       this.play();
       this.currentTime(instance.track.getActive().getData("videotimestart"));
    });
@@ -26,28 +40,29 @@ TrackLayer = function(map){
 
 TrackLayer.prototype = {
    init : function(){
-      // if (this.trackLayer){
-      //     this.map.removeLayer(this.trackLayer);
-      // }
-      
+      assertTrue(typeof styles.track === "object");
+      assertTrue(typeof styles.active === "object");
+
       this.trackLayer  = new OpenLayers.Layer.Vector("TrackLayer",{
 	 styleMap : new OpenLayers.StyleMap({
-	    "default" : trackStyle,
-	    "select" : activeStyle
+	    "default" : styles.track,
+	    "select" : styles.active
 	 }),
 	 projection  : geographic
       });
 
       
-      this.initControls();
+      this._initControls();
 
    },
+   /**
+    * @public
+    * @description Removes the trace layer from the graph layer
+    */
    destroy : function(){
-      // if (this.select){
-      //     this.select.deactivate();
-      // }
-      this.stopHighlight();
-      // this.map.removeControl(this.select);
+
+      this._stopHighlight();
+
       if (this.trackLayer){
 	 this.map.removeLayer(this.trackLayer);
 	 this.trackLayer = null;
@@ -56,28 +71,138 @@ TrackLayer.prototype = {
 	 this.map.removeLayer(this.activeLayer);
 	 this.activeLayer = null;
       }
-      mapLayer.enableControls();
    },
-   
-   initControls : function(){
-      if (this.select){
-	 this.map.removeControl(this.select);
-      }
-      this.select = new OpenLayers.Control.SelectFeature(this.trackLayer,{
-	 multiple : false,
-	 clickout : false,
-	 toggle : false,
-	 highlightOnly : true,
-	 onSelect : function(feature){
-	    console.log(feature);
-	    alert ("select tracklayer");
-	 },
+
+   /**
+    * @public
+    * @description Reinitialises the layer and draws the points
+    */
+   update : function(points){
+      assertTrue(points instanceof Array);
+      assertTrue(points.length > 0);
+      
+      this.init();
+      
+      var source = null,
+          track = new Trace(),
+          features = [];
+     
+      // build the new trace
+      points.forEach(function(point){
+	 track.add(point);
       });
+      
+      // draw the trace on the layer
+      this.trackLayer.addFeatures(track.toFeatures());
+      // add the trace-layer to the map
+      this.track = track;
+      this.map.addLayer(this.trackLayer);
+      this.map.setLayerIndex(this.trackLayer,999);
+
+      this._startVideo();
+   },
+   /** 
+    * @private
+    * @description Starts the video belonging to the current active segment
+    */
+   _startVideo : function(){
+      var start = this.track.getActive();
+
+      this.player.src({type:"video/ogg",src:start.getData("src")});
+      console.log("start at %d with %s",start.getData("videotimestart"),start);
+   },
+   /** 
+    * @private
+    * @description Removes the highlight listener from the video
+    */
+   _stopHighlight : function(){
+      this.player.removeEvent("timeupdate",this._highlightUpdate);
+   },
+   /** 
+    * @private
+    * @description Highlight the current segment of the trace-layer
+    */
+   _highlightUpdate : function(){
+      assertTrue(this instanceof TrackLayer);
+
+
+      var oldFeature = this.track.getActive(),
+          // will return the next active feature of the track
+          // this might be a track belonging to a different source
+          newFeature  = this.track.proceed(this.player.currentTime());
+
+
+      // console.log("newFeature",newFeature);
+      // inform others update the progress
+      if (oldFeature !== newFeature) {
+         onVideoProgress(newFeature, this.track.getNext());
+      }
+      // end of video
+      // don't remove listener yet -- this will be done in the layer destroy
+      if (newFeature === null){
+	 if (!this.isEnded){
+	    this._removeHighlight();
+	 }
+	 console.log("video ended");
+	 return;
+      } else if (oldFeature.getData("src") !== newFeature.getData("src")) { //change of videos
+	 console.log("video switched");
+	 // alert("switch");
+	 this.player.pause();
+	 this._stopHighlight();
+	 // restart video
+	 this._startVideo();
+	 return;
+      }
+      // video proceeds, highlight currently playing segment
+      if (oldFeature !== newFeature){
+	 console.log("video proceed");
+	 this._highlightFeature();
+      }
+   },
+   /**
+    * @private
+    * @description Highlights the current visited segment (Point or Connection) of the trace
+    */
+   _highlightFeature : function(){
+      this._removeHighlight();
+      this.ignoreSelect = true;
+      this.highlight.select(this.track.getActive().getVector());
+      this.ignoreSelect = false;
+      this.isEnded = false;
+   },
+   /**
+    * @private
+    * @description Remove the highlight from the layer.
+    */
+   _removeHighlight : function(){
+      this.highlight.unselectAll();
+      this.isEnded = true;
+   },
+   /**
+    * @private
+    */
+   _initControls : function(){
+      assert(this.trackLayer.CLASS_NAME, "OpenLayers.Layer.Vector");
 
       if (this.highlight){
 	 this.map.removeControl(this.highlight);
       }
+
+      // this will only hightlight the trace
+      // the highlight styles are defined in the trackLayer constructor
       this.highlight = new OpenLayers.Control.SelectFeature(this.trackLayer,{
+	 multiple : false,
+	 clickout : false,
+	 toggle : false,
+	 highlightOnly : true,
+      });
+
+      if (this.select){
+	 this.map.removeControl(this.select);
+      }
+      // add a navigation selection feature control
+      this.select = new OpenLayers.Control.SelectFeature(this.trackLayer,{
 	 multiple : false,
 	 clickout : false,
 	 toggle : false,
@@ -88,9 +213,9 @@ TrackLayer.prototype = {
 	    if (!instance.ignoreSelect){
 	       console.log("skipping...");
 
-	       srcOld = instance.track.getActive().getData("src");
-	       src = feature.geo_data.src;
-	       videotimestart = feature.geo_data.videotimestart;
+	       var srcOld = instance.track.getActive().getData("src"),
+	           src = feature.geo_data.src,
+	           videotimestart = feature.geo_data.videotimestart;
 
 	       //hit a mapconnection do nothing
 	       if (!videotimestart){
@@ -98,10 +223,10 @@ TrackLayer.prototype = {
 		  return;
 	       }
 	       //hit another video. set active and load video
-	       else if (srcOld != src){
-		  instance.stopHighlight();
+	       else if (srcOld !== src){
+		  instance._stopHighlight();
 		  instance.track.setActive(src,feature.geo_data.id);
-		  instance.startVideo();
+		  instance._startVideo();
 		  return;
 	       }
 	       //still the same video. proceed to the current time marker
@@ -114,98 +239,9 @@ TrackLayer.prototype = {
       });
 
       
-      this.map.addControl(this.select);
       this.map.addControl(this.highlight);
-      this.select.activate();
+      this.map.addControl(this.select);
       this.highlight.activate();
+      this.select.activate();
    },
-
-   highlightFeature : function(){
-      this.removeHighlight();
-      this.ignoreSelect = true;
-      this.highlight.select(this.track.getActive().getVector());
-      this.ignoreSelect = false;
-      this.isEnded = false;
-   },
-   removeHighlight : function(){
-      this.highlight.unselectAll();
-      this.isEnded = true;
-   },
-   //update reinitialises the layer and draws the points
-   update : function(points){
-      this.init();
-      
-      var source = null;
-      var track = new Trace();
-      var features = [];
-      points.forEach(function(point){
-	 track.add(point);
-      });
-
-      this.trackLayer.addFeatures(track.toFeatures());
-      this.track = track;
-      this.map.addLayer(this.trackLayer);
-      this.map.setLayerIndex(this.trackLayer,999);
-
-      this.startVideo();
-   },
-   startVideo : function(){
-      var start = this.track.getActive();
-
-      this.player.src({type:"video/ogg",src:start.getData("src")});
-      console.log("start at %d with %s",start.getData("videotimestart"),start);
-      // this.player.currentTime(start.getData("videotimestart"));
-
-   },
-   stopHighlight : function(){
-      this.player.removeEvent("timeupdate",this.highlightUpdate);
-   },
-   highlightUpdate : function(){
-      var instance = trackLayer;
-
-      oldFeature = instance.track.getActive();
-      //will return the next active feature of the track
-      //this might be a track belonging to a different source
-      // console.log("proceed to ",this.currentTime());
-      newFeature  = instance.track.proceed(this.currentTime());
-      // onVideoProgress(newFeature, null);
-      // console.log("newFeature",newFeature);
-      if (oldFeature !== newFeature) {
-         onVideoProgress(newFeature, instance.track.getNext());
-      }
-      //end of video
-      //dont remove listener yet -- this will be done in the layer destroy
-      if (newFeature == null){
-	 if (!instance.isEnded){
-	    instance.removeHighlight();
-	 }
-	 console.log("video ended");
-	 // alert ("end");
-	 // this.pause();
-	 return;
-      }
-      //change of videos
-      else if (oldFeature.getData("src") != newFeature.getData("src")){
-	 console.log("video switched");
-	 // alert("switch");
-	 this.pause();
-	 instance.stopHighlight();
-	 //restart video
-	 instance.startVideo();
-	 return;
-      }
-      //video proceeds, hightlight currently playing segment
-      if (oldFeature != newFeature){
-	 console.log("video proceed");
-	 instance.highlightFeature();
-      }
-   },
-   
-   // getTimeBuffered : function(){
-   // 	timeRange = this.player.buffered();
-   // 	buffered = timeRange.end(0) - timeRange.start(0);
-   // 	return buffered;
-   // }
-
-
 };
