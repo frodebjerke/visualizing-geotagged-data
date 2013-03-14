384670afff342c1a4511226b5b27854037933747
Frederik Claus <f.v.claus@googlemail.com>
2013-03-14 15:18:31 +0100

    js: added simple assert geo: moved osm styles to styles.js

---
+++ static/js/lib/assert.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+function croak (actual, expected, message) {
+   if (message) {
+      throw new Error(message);
+   } else {
+      throw new Error(actual + " is not " + expected + "!");
+   }
+}
+
+function assert (actual, expected, message) {
+   if (actual !== expected) {
+      croak(actual, expected, message);
+   }
+}
+
+function assertTrue (actual, message) {
+   if (actual !== true) {
+      croak(actual, true, message);
+   }
+}
---
+++ static/js/styles.js
@@ -0,0 +1,36 @@
+var styles = {
+   "default" : {
+      strokeWidth : 3,
+      strokeOpacity : 0.4,
+      strokeColor : "red",
+      pointRadius : 3,
+      pointColor : "red",
+      pointOpacity : 0.6,
+      fillColor : "red",
+
+   },
+
+   "selected" : {
+       strokeOpacity : 0.8,
+       pointColor : "green",
+       strokeColor : "green",
+       pointOpacity : 1,
+       fillColor : "green"
+    }
+};
+
+styles.track = {
+       pointColor : styles.selected.pointColor,
+       strokeColor : styles.selected.strokeColor,
+       fillColor : styles.selected.fillColor,
+       strokeWidth : styles.default.strokeWidth * 1.5,
+       pointRadius : styles.default.pointRadius * 1.5
+};
+
+styles.active = {
+       pointColor : "blue",
+       strokeColor : "blue",
+       fillColor : "blue",
+       strokeWidth : styles.track.strokeWidth,
+       pointRadius : styles.track.pointRadius
+};
